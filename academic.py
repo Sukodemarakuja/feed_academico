66d507f7cc694a27f91661e8844d5918fb79a3e9
Joao Suko Henrique <joojwallker@gmail.com>
2026-06-13 16:50:56 -0300

    Adicionando busca dinâmica com input, tratamento de espaços da URL, tratamento de listas vazias e melhorias visuais no terminal.

--- academic.py
+++ academic.py
@@ -4,7 +4,10 @@ from deep_translator import GoogleTranslator
 
 
 def rodar_bot():
-    url = "http://export.arxiv.org/api/query?search_query=all:videogames&max_results=3"
+
+    tema = input('Por qual tipo de artigo você esta pesquisando?')
+    tema_ajustado = tema.replace(" ", "+")
+    url = f"http://export.arxiv.org/api/query?search_query=all:{tema_ajustado}&max_results=3"
     resposta = requests.get(url)
     if resposta.status_code == 200:
         print('🟢Conectado ao ArXIV!')
@@ -13,19 +16,22 @@ def rodar_bot():
         sopa = BeautifulSoup(resposta.text, "xml")
         artigo = sopa.find_all("entry")
 
+        if len(artigo) == 0:
+            print("Nenhum artigo foi encontrado.")
+        
+        else:
+            for item in artigo:
+                procura_titulo = item.find("title")
+                titulo_limpo= procura_titulo.text.strip()
+                print("*"*20,titulo_limpo, "*"*20)
 
-        for item in artigo:
-            procura_titulo = item.find("title")
-            titulo_limpo= procura_titulo.text.strip()
-            print(titulo_limpo)
+                procura_resumo = item.find("summary")
+                resumo_limpo= procura_resumo.text.strip()
 
-            procura_resumo = item.find("summary")
-            resumo_limpo= procura_resumo.text.strip()
+                texto_pt=GoogleTranslator(source="en",
+                target='pt').translate(resumo_limpo)
 
-            texto_pt=GoogleTranslator(source="en",
-            target='pt').translate(resumo_limpo)
-
-            print(texto_pt)
+                print(f"\n {texto_pt}")
 
     else:
         print('❌Não foi possivel conectar.')
